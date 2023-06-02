5c7632117cbd62c8a699afbc3636d7e4a6a437f4
Max Andrew <maxsandrew@gmail.com>
2023-06-02 01:04:43 -0400

    Undo debug step

--- packages/frontend/src/components/Rebalancer/Modal.tsx
+++ packages/frontend/src/components/Rebalancer/Modal.tsx
@@ -79,7 +79,7 @@ export function RebalancerModal(props: RebalancerModalProps) {
     }
   }, [connectedNetworkId])
 
-  const [currentStep, setCurrentStep] = useState<number>(5)
+  const [currentStep, setCurrentStep] = useState<number>(0)
 
 
   /* HELPER FUNCTIONS */
